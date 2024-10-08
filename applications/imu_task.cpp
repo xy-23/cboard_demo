b23a0fcad326c4023b73495f87271430c034d404
XiaoYoung <xy_23@outlook.com>
2024-10-08 16:25:34 +0800

    add gyro reading and plot

--- applications/imu_task.cpp
+++ applications/imu_task.cpp
@@ -1,23 +1,27 @@
 #include "cmsis_os.h"
 #include "io/bmi088/bmi088.hpp"
+#include "io/plotter/plotter.hpp"
 
-io::BMI088 imu(&hspi1, GPIOA, GPIO_PIN_4, GPIOB, GPIO_PIN_0);
+io::Plotter plotter(&huart1);
+io::BMI088 bmi088(&hspi1, GPIOA, GPIO_PIN_4, GPIOB, GPIO_PIN_0);
 
-uint8_t imu_error_code;
+int plot_cnt = 0;
 
 extern "C" void imu_task()
 {
-  // do {
-  //   osDelay(100);
-  // } while (imu_error_code != 0);
-  osDelay(500);
-  imu_error_code = imu.init_acc();
-
-  if (imu_error_code != 0x00)
-    while (true);
+  osDelay(100);
+  bmi088.init();
 
   while (true) {
-    imu.update();
-    osDelay(10);
+    bmi088.update();
+    osDelay(1);
+
+    if (plot_cnt == 10) {
+      plot_cnt = 0;
+      plotter.plot(
+        bmi088.acc[0], bmi088.acc[1], bmi088.acc[2], bmi088.gyro[0], bmi088.gyro[1],
+        bmi088.gyro[2]);
+    }
+    plot_cnt++;
   }
 }
