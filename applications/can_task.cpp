05b54a1b043f9ffa4da52801856b65e08b1af090
XiaoYoung <xy_23@outlook.com>
2025-01-18 17:40:14 +0800

    test dm

--- applications/can_task.cpp
+++ applications/can_task.cpp
@@ -1,15 +1,40 @@
 #include "cmsis_os.h"
 #include "io/can/can.hpp"
-#include "motor/rm_motor/rm_motor.hpp"
+#include "io/dbus/dbus.hpp"
+#include "motor/dm_motor/dm_motor.hpp"
 
-sp::CAN can2(&hcan2);
+extern sp::DBus remote;
+
+sp::CAN can1(&hcan1);
+sp::DM_Motor motor_l(0x07, 0x17, 12.5, 20, 150);
+sp::DM_Motor motor_r(0x08, 0x18, 12.5, 20, 150);
 
 extern "C" void can_task()
 {
-  can2.config();
-  can2.start();
+  can1.config();
+  can1.start();
+
+  osDelay(1000);
+  motor_l.write_enable(can1.tx_data);
+  can1.send(motor_l.tx_id);
+  motor_r.write_enable(can1.tx_data);
+  can1.send(motor_r.tx_id);
 
   while (true) {
+    if (remote.sw_r == sp::DBusSwitchMode::DOWN) {
+      motor_l.cmd(0);
+      motor_r.cmd(0);
+    }
+    else {
+      motor_l.cmd(remote.sw_l == sp::DBusSwitchMode::MID ? -80 : 0);
+      motor_r.cmd(remote.sw_l == sp::DBusSwitchMode::MID ? 80 : 0);
+    }
+
+    motor_l.write(can1.tx_data);
+    can1.send(motor_l.tx_id);
+    motor_r.write(can1.tx_data);
+    can1.send(motor_r.tx_id);
+
     osDelay(1);
   }
 }
@@ -18,9 +43,12 @@ extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef * hcan)
 {
   auto stamp_ms = osKernelSysTick();
 
-  if (hcan == &hcan2) {
-    can2.recv();
+  if (hcan == &hcan1) {
+    can1.recv();
 
-    // if (can2.rx_id == motor_yaw.rx_id) motor_yaw.read(can2.rx_data, stamp_ms);
+    if (can1.rx_id == motor_l.rx_id)
+      motor_l.read(can1.rx_data, stamp_ms);
+    else if (can1.rx_id == motor_r.rx_id)
+      motor_r.read(can1.rx_data, stamp_ms);
   }
 }
--- applications/plot_task.cpp
+++ applications/plot_task.cpp
@@ -1,24 +1,21 @@
 #include "cmsis_os.h"
 #include "io/bmi088/bmi088.hpp"
 #include "io/plotter/plotter.hpp"
-#include "motor/rm_motor/rm_motor.hpp"
-#include "referee/pm02/pm02.hpp"
+#include "motor/dm_motor/dm_motor.hpp"
 #include "tools/mahony/mahony.hpp"
 
 extern sp::BMI088 bmi088;
 extern sp::Mahony imu;
-
-extern sp::RM_Motor motor_yaw;
-extern float cmd_voltage;
-
-extern sp::PM02 pm02;
+extern sp::DM_Motor motor_l;
+extern sp::DM_Motor motor_r;
 
 sp::Plotter plotter(&huart1);
 
 extern "C" void plot_task()
 {
   while (true) {
-    // plotter.plot(pm02.power_heat_count);
+    plotter.plot(
+      motor_l.speed, motor_r.speed, motor_l.error, -motor_r.error, motor_l.torque, motor_r.torque);
     osDelay(10);
   }
 }
