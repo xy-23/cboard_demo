854bcf771c7a9cdbe2f5d59df45cf681ff7f8aa6
XiaoYoung <xy_23@outlook.com>
2025-02-12 16:00:28 +0800

    sync sp_middleware

--- applications/uart_task.cpp
+++ applications/uart_task.cpp
@@ -3,13 +3,15 @@
 #include "referee/pm02/pm02.hpp"
 
 sp::DBus remote(&huart3);
+sp::PM02 pm02(&huart6);
 
 extern "C" void uart_task()
 {
   remote.request();
+  pm02.request();
 
   while (true) {
-    osDelay(10);
+    osDelay(33);  // 30Hz
   }
 }
 
@@ -21,11 +23,20 @@ extern "C" void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef * huart, uint16_t 
     remote.update(Size, stamp_ms);
     remote.request();
   }
+
+  else if (huart == pm02.huart) {
+    pm02.update(Size);
+    pm02.request();
+  }
 }
 
 extern "C" void HAL_UART_ErrorCallback(UART_HandleTypeDef * huart)
 {
   if (huart == &huart3) {
     remote.request();
+  }
+
+  else if (huart == pm02.huart) {
+    pm02.request();
   }
 }
