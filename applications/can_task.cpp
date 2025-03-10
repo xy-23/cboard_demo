b75f21aa0ee4053ab83d37c898ddb2cb88ee5e6e
XiaoYoung <xy_23@outlook.com>
2025-03-10 22:31:35 +0800

    test diff_drive

--- applications/can_task.cpp
+++ applications/can_task.cpp
@@ -3,6 +3,12 @@
 #include "io/dbus/dbus.hpp"
 #include "motor/lk_motor/lk_motor.hpp"
 #include "motor/rm_motor/rm_motor.hpp"
+#include "tools/diff_drive/diff_drive.hpp"
+#include "tools/pid/pid.hpp"
+
+constexpr float T = 1e-3f;   // s
+constexpr float R = 0.06f;   // m
+constexpr float W = 0.435f;  // m
 
 extern sp::DBus remote;
 
@@ -11,6 +17,11 @@ sp::CAN can2(&hcan2);
 
 sp::LK_Motor motor_l(1);
 sp::LK_Motor motor_r(2);
+
+sp::PID motor_l_speed_pid(T, 1, 0, 0, 5, 5);
+sp::PID motor_r_speed_pid(T, 1, 0, 0, 5, 5);
+
+sp::DiffDrive diff_drive(W / 2, R, true, false);
 
 extern "C" void can_task()
 {
@@ -21,8 +32,20 @@ extern "C" void can_task()
   can2.start();
 
   while (true) {
-    motor_l.cmd(remote.ch_lv);
-    motor_r.cmd(-remote.ch_rv);
+    diff_drive.update(motor_l.speed, motor_r.speed);
+    diff_drive.calc(remote.ch_rv * 2, -remote.ch_lh * 13);
+
+    motor_l_speed_pid.calc(diff_drive.speed_l, motor_l.speed);
+    motor_r_speed_pid.calc(diff_drive.speed_r, motor_r.speed);
+
+    if (remote.sw_r == sp::DBusSwitchMode::DOWN) {
+      motor_l.cmd(0);
+      motor_r.cmd(0);
+    }
+    else {
+      motor_l.cmd(motor_l_speed_pid.out);
+      motor_r.cmd(motor_r_speed_pid.out);
+    }
 
     motor_l.write(can1.tx_data);
     motor_r.write(can1.tx_data);
--- applications/plot_task.cpp
+++ applications/plot_task.cpp
@@ -2,21 +2,39 @@
 #include "io/bmi088/bmi088.hpp"
 #include "io/plotter/plotter.hpp"
 #include "motor/dm_motor/dm_motor.hpp"
+#include "motor/lk_motor/lk_motor.hpp"
 #include "motor/rm_motor/rm_motor.hpp"
+#include "referee/pm02/pm02.hpp"
+#include "tools/diff_drive/diff_drive.hpp"
 #include "tools/mahony/mahony.hpp"
+#include "tools/pid/pid.hpp"
 
 extern sp::BMI088 bmi088;
 extern sp::Mahony imu;
 
-extern sp::RM_Motor motor_yaw;
-extern sp::RM_Motor motor_pitch;
+extern sp::PM02 pm02;
+
+extern sp::PID motor_l_speed_pid;
+extern sp::PID motor_r_speed_pid;
+
+extern sp::LK_Motor motor_l;
+extern sp::LK_Motor motor_r;
+
+extern sp::DiffDrive diff_drive;
 
 sp::Plotter plotter(&huart1);
+
+float power_computed = 0;
 
 extern "C" void plot_task()
 {
   while (true) {
-    plotter.plot(bmi088.temp);
+    // plotter.plot(motor_l_speed_pid.data.set, motor_l_speed_pid.data.fdb, motor_l_speed_pid.out);
+    // plotter.plot(motor_r_speed_pid.data.set, motor_r_speed_pid.data.fdb, motor_r_speed_pid.out);
+
+    power_computed = motor_l.torque * motor_l.speed + motor_r.torque * motor_r.speed;
+
+    plotter.plot(pm02.power_heat.reserved_3, power_computed, diff_drive.v, diff_drive.w);
     osDelay(10);
   }
 }
