27fcba8b94963de294d12a55f5b619150d8143ba
XiaoYoung <xy_23@outlook.com>
2025-03-10 21:20:59 +0800

    add lk_motor

--- applications/can_task.cpp
+++ applications/can_task.cpp
@@ -1,6 +1,7 @@
 #include "cmsis_os.h"
 #include "io/can/can.hpp"
 #include "io/dbus/dbus.hpp"
+#include "motor/lk_motor/lk_motor.hpp"
 #include "motor/rm_motor/rm_motor.hpp"
 
 extern sp::DBus remote;
@@ -8,8 +9,8 @@ extern sp::DBus remote;
 sp::CAN can1(&hcan1);
 sp::CAN can2(&hcan2);
 
-sp::RM_Motor motor_yaw(1, sp::RM_Motors::GM6020_V, 1, false);
-sp::RM_Motor motor_pitch(2, sp::RM_Motors::GM6020_V, 1, false);
+sp::LK_Motor motor_l(1);
+sp::LK_Motor motor_r(2);
 
 extern "C" void can_task()
 {
@@ -20,20 +21,12 @@ extern "C" void can_task()
   can2.start();
 
   while (true) {
-    if (remote.sw_r == sp::DBusSwitchMode::DOWN) {
-      motor_yaw.cmd(0);
-      motor_pitch.cmd(0);
-    }
-    else {
-      motor_yaw.cmd(remote.ch_rh * 24);
-      motor_pitch.cmd(remote.ch_rv * 24);
-    }
+    motor_l.cmd(remote.ch_lv);
+    motor_r.cmd(-remote.ch_rv);
 
-    motor_yaw.write(can2.tx_data);
-    can2.send(motor_yaw.tx_id);
-
-    motor_pitch.write(can1.tx_data);
-    can1.send(motor_pitch.tx_id);
+    motor_l.write(can1.tx_data);
+    motor_r.write(can1.tx_data);
+    can1.send(motor_l.tx_id);
 
     osDelay(1);
   }
@@ -41,19 +34,21 @@ extern "C" void can_task()
 
 extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef * hcan)
 {
-  auto stamp_ms = osKernelSysTick();
+  // auto stamp_ms = osKernelSysTick();
 
   while (HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0) > 0) {
     if (hcan == &hcan1) {
       can1.recv();
 
-      if (can1.rx_id == motor_pitch.rx_id) motor_pitch.read(can1.rx_data, stamp_ms);
+      if (can1.rx_id == motor_l.rx_id)
+        motor_l.read(can1.rx_data);
+
+      else if (can1.rx_id == motor_r.rx_id)
+        motor_r.read(can1.rx_data);
     }
 
     else if (hcan == &hcan2) {
       can2.recv();
-
-      if (can2.rx_id == motor_yaw.rx_id) motor_yaw.read(can2.rx_data, stamp_ms);
     }
   }
 }
